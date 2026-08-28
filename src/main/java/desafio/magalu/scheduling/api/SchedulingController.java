8bbac1dd30e1aa4ba2abd63166a44d8f7f1828f9
raphael720 <raphaelleite720@gmail.com>
2026-08-27 23:27:41 -0300

    creation of the controller calls

--- src/main/java/desafio/magalu/scheduling/api/SchedulingController.java
+++ src/main/java/desafio/magalu/scheduling/api/SchedulingController.java
@@ -1,13 +1,36 @@
 package desafio.magalu.scheduling.api;
 
+import desafio.magalu.scheduling.api.dtos.SchedulingRequestDTO;
+import desafio.magalu.scheduling.api.dtos.SchedulingResponseDTO;
+import desafio.magalu.scheduling.application.SchedulingService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/scheduling")
+@RequiredArgsConstructor
 public class SchedulingController {
 
+    private  final SchedulingService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SchedulingResponseDTO> getScheduling(@PathVariable Long id) {
+        var schedulingOfId = this.service.findById(id);
+        return ResponseEntity.ok(schedulingOfId);
+    }
+
+    @PostMapping
+    public ResponseEntity<SchedulingResponseDTO> createScheduling(@Valid @RequestBody SchedulingRequestDTO schedulingDTO) {
+        var newScheduling = this.service.createScheduling(schedulingDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newScheduling);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteScheduling(@PathVariable Long id) {
+        this.service.deleteScheduling(id);
+        return ResponseEntity.noContent().build();
+    }
 }
